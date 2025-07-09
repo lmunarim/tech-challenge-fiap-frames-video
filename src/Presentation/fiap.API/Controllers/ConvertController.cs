eb4186cb7fd9fee74ca71f1f15b8e3a0ebab887c
lmunarim <lmunarim@gmail.com>
2025-07-09 00:27:53 -0300

    teste

--- src/Presentation/fiap.API/Controllers/ConvertController.cs
+++ src/Presentation/fiap.API/Controllers/ConvertController.cs
@@ -30,12 +30,12 @@ namespace fiap.API.Controllers
                     return BadRequest(new { success = false, message = "Unsupported format" });
 
                 var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss"); // mudar guid
-                var videoFile = $@"./uploads/{timestamp}_{Path.GetFileName(video.FileName)}";
+                var videoFile = $@"/app/uploads/{timestamp}_{Path.GetFileName(video.FileName)}";
 
                 using (var stream = System.IO.File.Create(videoFile))
                     await video.CopyToAsync(stream);
 
-                var tempDir = $@"./temporary/{timestamp}";
+                var tempDir = $@"/app/temporary/{timestamp}";
 
                 if (!Directory.Exists(tempDir))
                     Directory.CreateDirectory(tempDir);
@@ -52,7 +52,7 @@ namespace fiap.API.Controllers
                     return BadRequest(new { success = false, message = "No frames extracted" });
 
                 var zipName = $"frames_{timestamp}.zip";
-                var zipPath = $@"./outputs/{zipName}";
+                var zipPath = $@"/app/outputs/{zipName}";
                 ZipFile.CreateFromDirectory(tempDir, zipPath);
 
                 Directory.Delete(tempDir, true);
