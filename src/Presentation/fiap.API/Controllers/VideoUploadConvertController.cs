e34e5d08f667bdf7cd60a52df6a1a9033bb222c0
lmunarim <lmunarim@gmail.com>
2025-07-11 22:20:36 -0300

    ts

--- src/Presentation/fiap.API/Controllers/VideoUploadConvertController.cs
+++ src/Presentation/fiap.API/Controllers/VideoUploadConvertController.cs
@@ -70,7 +70,8 @@ namespace fiap.API.Controllers
                     return BadRequest(new { success = false, message = "No video uploaded" });
                 }
 
-                _ = Task.Run( async() => await _videoUploadApplication.ConverterVideoFrames(video, videoUpload));
+                /// _ = Task.Run( async() => await _videoUploadApplication.ConverterVideoFrames(video, videoUpload));
+                await _videoUploadApplication.ConverterVideoFrames(video, videoUpload);
 
 
                 return new CreatedResult(videoUpload.UrlS3, new
